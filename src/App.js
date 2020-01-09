a491182ba53f997dbb0e10faa4b395ea2576bc74
Martinezm87 <Martinezm0087@gmail.com>
2020-01-09 16:39:33 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './components/SignIn.js';
+import SignUp from './components/SignUp.js';
+import * as yup from 'yup';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <BrowserRouter>
+
+        <Route exact path="/" component={SignIn} />
+        <Route path ="/signup" component={SignUp} />
+      </BrowserRouter>
+      </header>
+    </div>
+  );
+}
+
+export default App;
